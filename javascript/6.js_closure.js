244c0a9d6e0f0889650b4ce7ef060ffb2d95738f
taoxiaomeng <514570778@qq.com>
2021-07-22 13:48:45 +0800

    更新

---
+++ javascript/6.js_closure.js
@@ -0,0 +1,41 @@
+/**
+ * 闭包的理解
+ */
+
+ function func1(){
+     var n = 999;
+ }
+ //console.log(n)   // n is not defined
+
+function func(){
+    var n = 999;
+    add = function(){
+        n++
+    }
+    // 闭包show函数
+    function show(){
+        return n
+    }
+    return show;
+}
+var tempShow = func();
+console.log(tempShow());
+add();
+console.log(tempShow());
+
+
+var name = "The window";
+var object = {
+    name: "my object",
+    getNameFunc : function(){
+       // var that = this;
+        return function(){
+            return this.name
+          // return that.name
+        }
+    }
+}
+
+var temp = object.getNameFunc().bind(object);
+console.log(temp()) // the window
+
